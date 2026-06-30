073a68fa1304eb165a989158f4b56d5827ffc187
François Villemot <francois.villemot@espci.fr>
2026-06-30 10:02:45 +0200

    chore: more public functions

--- src/seminars/__init__.py
+++ src/seminars/__init__.py
@@ -1,4 +1,4 @@
-from seminars.db import open_or_create_db
+from seminars.db import insert_speaker, open_or_create_db, read_speakers
 from seminars.models import Speaker
 
-__all__ = ["open_or_create_db", "Speaker"]
+__all__ = ["open_or_create_db", "Speaker", "insert_speaker", "read_speakers"]
